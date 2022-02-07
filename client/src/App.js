6e154cd4b17da1c952e6621dfcf3647750b6b237
Dattatraya Magar <dattamagar211@gmail.com>
2022-02-07 21:48:38 +0530

    client modified

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
+import Register from "./pages/register/Register";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" >
+         <Home />
+        </Route>
+       <Route path="/login">
+        <Login />
+       </Route>
+       <Route path="/register" />
+        <Register />
+       <Route path="/profile/:username">
+        <Profile />
+       </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
